f8f46f701204758598d4aa5555efeaa4e57917ec
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-15 11:31:32 -0300

    Curso Introdutório

---
+++ Modulo_02/Criando_Variaveis/ex001.js
@@ -0,0 +1,24 @@
+/*
+Bom, em resumo ambos fazem o mesmo trabalho de criar as variáveis.
+Porém, ambos possuem algumas diferenças, com as quais iremos abordar um
+pouco mais à frente.
+*/
+
+let pizzaDeQueijo = 10
+var pizzaDeFrango = 20
+
+/*
+Após escrever o nome da variável, usamos um sinal de atribuição (=)
+caracterizado pelo símbolo de igual, e na sequência a informação que queremos
+atribuir.
+
+Na verdade é que todo conteúdo em texto que for atribuído a uma
+variável deve ficar entre aspas, assim o computador entende que tudo que
+estiver entre elas será considerado como conteúdo.
+
+Não! Essa regra só é válida para conteúdos de textos. Se o conteúdo que
+você armazenar for um número, você pode atribuir sem aspas!
+*/
+
+var nome = "Cesar Michelin"
+var idade = 27
