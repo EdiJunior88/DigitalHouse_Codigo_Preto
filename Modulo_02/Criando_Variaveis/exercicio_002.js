4af88e7768cf5547b6992e1775e33e272c1560fa
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-15 11:46:53 -0300

    Curso Introdutório

---
+++ Modulo_02/Criando_Variaveis/exercicio_002.js
@@ -0,0 +1,8 @@
+/*
+Declare 4 variáveis: nome, sobrenome, numeroDaSorte, idade e atribua seus respectivos valores nas variáveis.
+*/
+
+var nome = "Edivaldo"
+var sobrenome = "Junior"
+var numeroDaSorte = 26
+var idade = 33
