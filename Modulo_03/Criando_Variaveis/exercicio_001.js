819fbf417ad5c6e5bdb6d7bb4add1066b831356b
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-15 15:34:26 -0300

    Criando variáveis

---
+++ Modulo_03/Criando_Variaveis/exercicio_001.js
@@ -0,0 +1,8 @@
+/*
+Vamos escrever nossas primeiras variáveis juntos. Você irá usar tudo que aprendeu anteriormente!
+
+Declare duas variáveis chamadas  idade  e  peso,  e atribua um valor numérico a ambas.
+*/
+
+var idade = 33
+var peso = 96.5
---
+++ Modulo_03/Criando_Variaveis/exercicio_002.js
@@ -0,0 +1,10 @@
+/*
+Agora para deixar todo esse conhecimento fixo na mente, precisamos praticar um pouco mais!
+
+Declare 4 variáveis: nome, sobrenome, numeroDaSorte, idade e atribua seus respectivos valores nas variáveis.
+*/
+
+var nome = "Edivaldo"
+var sobrenome = "Junior"
+var numeroDaSorte = 26
+var idade = 33
