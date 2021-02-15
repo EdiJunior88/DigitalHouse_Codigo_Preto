3d6e4c300b22f1d1b7b2b743bfac644487025e86
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-15 15:45:08 -0300

    Usando Variáveis

---
+++ Modulo_03/Usando_Variaveis/exercicio_001.js
@@ -0,0 +1,17 @@
+/*
+Precisamos criar um código que gera cartão de visita com nome, sobrenome e profissão.
+Crie uma variável para guardar cada informação, depois faça a concatenação das informações
+numa variável chamada cartaoDeVisita.
+
+Exemplo:
+nome: Natalia
+sobrenome: Lira
+profissão: Programadora
+*/
+
+var nome = "Luíz"
+var sobrenome = "Pedróvisk"
+var profissao = "Engenheiro de Obras"
+var cartaoDeVisita = nome + " " + sobrenome + " - " + profissao
+
+console.log("Cartão de Visita: " + cartaoDeVisita)
