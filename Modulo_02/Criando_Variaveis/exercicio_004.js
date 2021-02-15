1cccf92314c5de281412f91c6a5c42dcc5708d39
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-15 13:39:52 -0300

    Criando variáveis

---
+++ Modulo_02/Criando_Variaveis/exercicio_004.js
@@ -0,0 +1,10 @@
+/*
+Declare cinco variáveis com os seguintes dados: seu nome, idade, altura, solteiro e
+seriesFavoritas utilizando os tipos de dados vistos.
+*/
+
+var nome = "Edivaldo"
+var idade = 33
+var altura = 1.76
+var solteiro = true
+var seriesFavoritas = ["Dr. House", "SmallVile", "Um maluco no pedaço", "The Office"]
