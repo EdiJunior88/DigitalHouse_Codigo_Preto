64707e314975e5bdb2ddc9b3a850078503a67318
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-15 15:40:50 -0300

    Tipos de Dados

---
+++ Modulo_03/Tipos_de_Dados/exercicio_001.js
@@ -0,0 +1,12 @@
+/*
+Escreva no script as variáveis expostas abaixo substituindo os tipos de
+dados por valores, conforme vimos anteriormente.
+
+var idade = Number
+var sobrenome = String
+var salarioMinimo = Float
+*/
+
+var idade = 33
+var sobrenome = "Junior"
+var salarioMinimo = 1200.56
---
+++ Modulo_03/Tipos_de_Dados/exercicio_002.js
@@ -0,0 +1,12 @@
+/*
+Vamos praticar a declaração dos tipos de dados que aprendemos.
+
+Declare cinco variáveis com os seguintes dados: seu nome, idade, altura,
+solteiro e seriesFavoritas utilizando os tipos de dados vistos.
+*/
+
+var nome = "Edivaldo"
+var idade = 33
+var altura = 1.76
+var solteiro = true
+var seriesFavoritas = ["Dr. House", "The Office", "Um Maluco no Pedaço"]
