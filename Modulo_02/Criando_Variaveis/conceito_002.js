56db7fb9af31227bb97827b6f5866d4d7d20b33f
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-15 13:32:04 -0300

    Criando variáveis

---
+++ Modulo_02/Criando_Variaveis/conceito_002.js
@@ -0,0 +1,45 @@
+/*
+String: são sequências de caracteres que devem ser mostradas entre
+aspas simples ou duplas. Podemos dizer que as strings são textos que
+desejamos imprimir no código, por isso usamos aspas para que a cadeia
+de caracteres que desejamos seja impressa.
+*/
+
+var nome = "Camila"
+var sobrenome = "Pinheiro"
+var frase = 'Meu nome é camila'
+
+/*
+Number: são os dados numéricos positivos ou negativos inteiros.
+Quando usamos o number, não precisamos utilizar aspas pois o nosso
+código aceita o número da forma como declaramos.
+*/
+
+var nascimento = 1988
+var idade = 22
+
+/*
+Float: são dados numéricos positivos ou negativos decimais. Ao contrário
+das regras matemáticas que aprendemos na escola, aqui utilizamos o
+ponto em números decimais.
+*/
+
+var valor = 120.20
+var preco = 1.99
+
+/*
+Boolean: tipo de dado que só tem dois valores possíveis: true ou false.
+Usamos os booleanos em situações de afirmação, ou seja, quando
+precisamos dizer se uma coisa é verdadeira ou falsa.
+*/
+
+var luzApagada = false
+var vacinasEmDia = true
+
+/*
+Array: são listas ou conjuntos capazes de guardar diversos dados ao
+mesmo tempo em uma única variável.
+*/
+
+var numerosMega = [10, 23, 2, 8, 30, 40, 4, 19, 42]
+var comidas = ["Sushi", "Feijoada", "Strogonoff", "Hamburgue", "Kebab", "Batata Frita"]
