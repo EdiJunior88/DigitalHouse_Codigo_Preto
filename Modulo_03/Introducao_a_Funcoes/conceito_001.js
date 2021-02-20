f7e5fc27849082e387d04e324db35ced374b9ddc
EdiJunior88 <er.moura88@yahoo.com.br>
2021-02-19 21:16:30 -0300

    Introdução a Funções

---
+++ Modulo_03/Introducao_a_Funcoes/conceito_001.js
@@ -0,0 +1,16 @@
+/*
+Funções é uma forma de encapsular um código para utilizarmos. Assim,
+iremos criar soluções a partir da definição do que é necessário para o método
+realizá-las, ou seja, definimos instruções para o computador realizar.
+
+Poderíamos pensar em uma função chamada saudação , em que iremos
+configurar para que o computador exiba um console.log(“olá tudo bem”) sempre
+que for executada.
+
+Mas vale ressaltar que podemos fazer qualquer instrução dentro da função,
+como loops condições e afins!
+*/
+
+function saudacao(){
+    console.log("Olá tudo bem")
+}
